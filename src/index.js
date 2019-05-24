b3e401bfcaea0e3436a3b0908415d413d8e64ef5
谈华宇 <tanhy@77ircloud.com>
2019-05-24 15:39:52 +0800

    feat: basic config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
+
 function component() {
   var element = document.createElement("div");
 
-  element.innerHTML = _.join(["Hello", "webpack"], "");
+  element.innerHTML = _.join(["Hello", "webpack"], ",");
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,8 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  output: path.resolve(__dirname,'dist')
+  output: {
+    filename: 'bundle.js',
+    path:path.resolve(__dirname, 'dist')
+  }
 }
