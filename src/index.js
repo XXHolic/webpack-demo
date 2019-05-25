8f0c16f54ac7ef97e146aa3d7305cbd969b2743d
谈华宇 <tanhy@77ircloud.com>
2019-05-26 02:45:53 +0800

    feat: try tree shake but not not same

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import _ from "lodash";
-import printMe from "./print.js";
+// import _ from "lodash";
+// import printMe from "./print.js";
+import { cube, square } from "./math.js";
 import './style.css';
 
 function component() {
-  var element = document.createElement("div");
+  var element = document.createElement("pre");
   var btn = document.createElement("button");
 
-  element.innerHTML = _.join(["Hello", "webpack"], ",");
+  // element.innerHTML = _.join(["Hello", "webpack"], ",");
 
-  btn.innerHTML = "click me an check the console";
-  btn.onclick = printMe;
+  btn.innerHTML = [
+    'Hello webpack',
+    '5 cubed is equal to '+cube(5)
+  ].join('\n\n');
+  // btn.onclick = printMe;
 
-  element.appendChild(btn);
+  // element.appendChild(btn);
 
   return element;
 }
@@ -20,12 +24,12 @@ function component() {
 let element = component();
 document.body.appendChild(element);
 
-if (module.hot) {
-  module.hot.accept("./print.js", function() {
-    console.log("Accept the updated printMe module");
-    document.body.remove(element);
-    element = component();
-    document.body.appendChild(element);
-    // printMe();
-  });
-}
+// if (module.hot) {
+//   module.hot.accept("./print.js", function() {
+//     console.log("Accept the updated printMe module");
+//     document.body.remove(element);
+//     element = component();
+//     document.body.appendChild(element);
+//     // printMe();
+//   });
+// }
