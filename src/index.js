4f37eb90eee03652bbddac55b365600e39f6c1a6
谈华宇 <tanhy@77ircloud.com>
2019-05-25 14:27:01 +0800

    chore: prepare for next chapter

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml'
 
 function component() {
   var element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], ",");
-  element.classList.add('hello');
-
-  var myIcon = new Image();
-  myIcon.src=Icon;
-  myIcon.classList.add('logo');
-
-  element.appendChild(myIcon);
-
-  var fontEle = document.createElement("span");
-  fontEle.innerHTML = "&#xe644;";
-  fontEle.className='icon iconfont';
-
-  element.appendChild(fontEle);
-
-  console.info('Data:', Data);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,35 +5,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path:path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules:[{
-      test:/\.css$/,
-      use:[
-        'style-loader',
-        'css-loader'
-      ]
-    },
-    {
-      test:/\.(png|svg|jpg|gif)$/,
-      use:[
-        'file-loader'
-      ]
-    },
-    {
-      test:/\.(eot|woff|ttf|otf)$/,
-      use:[
-        'file-loader'
-      ]
-    },
-    {
-      test: /\.(csv|tsv)$/,
-      use:['csv-loader']
-    },
-    {
-      test: /\.xml$/,
-      use:['xml-loader']
-    },
-  ]
   }
 }
