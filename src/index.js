f51aabefdb5481287d128dff6c481f4551cd99e4
谈华宇 <tanhy@77ircloud.com>
2019-05-25 14:48:27 +0800

    feat: multi entry

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from 'lodash';
+import printMe from "./print.js"
 
 function component() {
   var element = document.createElement("div");
+  var btn = document.createElement('button');
 
   element.innerHTML = _.join(["Hello", "webpack"], ",");
+
+  btn.innerHTML = "click me an check the console";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
